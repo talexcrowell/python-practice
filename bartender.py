bec4d4cb327b5261ec07c13b09c1cffd79f12760
talexcrowell <talexcrowell@gmail.com>
2018-12-31 12:57:57 -0800

    starting bartender.py

---
+++ bartender.py
@@ -0,0 +1,16 @@
+questions = {
+  "strong":"Do ye like yer drinks strong?",
+  "salty": "Do ye like it with a salty tang?",
+  "bitter": "Are ye a lubber who likes it bitter?",
+  "sweet": "Would ye like a bit of sweetness with yer poison?",
+  "fruity": "Are ye one for a fruity finish?",
+}
+
+ingredients = {
+  "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+  "salty": ["olive on a stick", "salt dusted rim", "rasher of bacon"],
+  "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+  "sweet": ["sugar cube", "spoonful of honey", "splash of cola"],
+  "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+}
+
