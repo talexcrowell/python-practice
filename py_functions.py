4a6bf43602eea301d1a23bdb8e922851134ee741
talexcrowell <talexcrowell@gmail.com>
2018-12-31 12:48:11 -0800

    finished function examples

--- py_functions.py
+++ py_functions.py
@@ -8,5 +8,22 @@ def print_function():
   '''I'm also a function, but I don't take any parameters'''
   print("I'm {}, and I'm printing now".format(print_function.__name__))
 
-if __name__ == '__main__':
+def function3(a=55, b=22):
+  '''I'm a function that calls other functions'''
+  print("I'm {} and I'm about to call subtractor function".format(function3.__name__))
+  total = subtractor(a,b)
+  print("I'm {} and I'm about to call print_function".format(function3.__name__))
   print_function()
+  print("I'm {} and I'm about to return total".format(function3.__name__))
+  return total
+
+def side_effect_test(value):
+  value[1] = 'orange'
+  print("Inside the function, the value becomes {}".format(value))
+
+
+if __name__ == '__main__':
+  value = ['red', 'blue', 'white']
+  print('Outside the function, the value starts as {}'.format(value))
+  side_effect_test(value)
+  print('Outside the function, the value is now {}'.format(value))
